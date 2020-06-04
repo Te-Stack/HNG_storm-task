7761ad7b71b4f25a98f12beb5b98e5b885767f58
Quincy Oghenetejiri <64927592+Te-Stack@users.noreply.github.com>
2020-06-04 11:56:10 +0100

    Add files via upload
    My_Scripts

---
+++ scripts/Quincy.py
@@ -0,0 +1,11 @@
+def task():
+    name = "Quincy Oghenetejiri" 
+    Id = "HNG-00954" 
+    language = "Python"
+    email = "ukumakubequincy@gmail.com"
+
+    return f"Helllo World, this is {name} with HNGi7 ID {Id} using python for stage 2 task. {email} "
+
+
+print(task())
+
